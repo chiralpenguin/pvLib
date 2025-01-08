de99ac7cbe898ac13315a1a3c3f976ec422415c5
chiralpenguin <50334616+chiralpenguin@users.noreply.github.com>
2025-01-08 14:54:16 +0000

    Update paper repo URL

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ version = "1.0"
 
 repositories {
     mavenCentral()
-    maven("https://papermc.io/repo/repository/maven-public/")
+    maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 dependencies {
