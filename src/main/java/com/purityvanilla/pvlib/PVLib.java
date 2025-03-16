9241b33a08bb9962e927074c69f3676e4653eaff
chiralpenguin <50334616+chiralpenguin@users.noreply.github.com>
2025-03-16 22:05:07 +0000

    Minor refactoring

--- src/main/java/com/purityvanilla/pvlib/PVLib.java
+++ src/main/java/com/purityvanilla/pvlib/PVLib.java
@@ -2,7 +2,7 @@ package com.purityvanilla.pvlib;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class pvLib extends JavaPlugin {
+public class PVLib extends JavaPlugin {
 
     @Override
     public void onEnable() {
